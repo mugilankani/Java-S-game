201b6834f229d7c2720e81eed1455314a52f7765
Mugilan <mugilankani951@gmail.com>
2023-12-13 20:33:58 -0800

    half of the functionality was over

---
+++ game.js
@@ -0,0 +1,50 @@
+const players = document.querySelector(".players")
+
+// document.addEventListener('keydown', function(event) {
+//   const key = event.key; // 'a' or 'l'
+//   const style = window.getComputedStyle(players);
+//   const left = parseInt(style.left, 10);
+// let leftCount = 0;
+
+// let rightCount = 0;
+
+//   if (key === 'l') {
+//       players.style.left = (left - 10) + 'px'; // move left
+//       leftCount +=10
+//   } else if (key === 'a') {
+//       players.style.left = (left + 10) + 'px'; // move right
+//       rightCount +=10;
+//   }
+// });
+let moveInterval;
+
+let leftCount = 0
+let rightCount =0
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key; // 'a' or 'l'
+    const style = window.getComputedStyle(players);
+    let left = parseInt(style.left.replace('px', ''), 10);
+
+    if (moveInterval) {
+        clearInterval(moveInterval);
+    }
+  
+    if (key === 'l') {
+        moveInterval = setInterval(function() {
+            left += 10;
+            players.style.left = left + 'px'; // move left
+        }, 100);
+    } else if (key === 'a') {
+        moveInterval = setInterval(function() {
+            left -= 10;
+            players.style.left = left + 'px'; // move right
+        }, 100);
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if (moveInterval) {
+        clearInterval(moveInterval);
+    }
+});
